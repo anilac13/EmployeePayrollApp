a558e086c8bc1ecf66361bd5bd349407d1db1f8b
anilac13 <anil4u2496@gmail.com>
2023-06-26 21:31:30 +0530

    [Anil] Add:View employee payroll data from Local storage

--- js/home.js
+++ js/home.js
@@ -1,11 +1,16 @@
+let empPayrollList;
+
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmpDataFromLocalStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
 
 const createInnerHtml = () => {
     const headerHtml = "<th>Profile pic</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>NOTES</th><th>Actions</th>";
     let innerHtml = `${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJson();
+    //let empPayrollList = createEmployeePayrollJson();
     for (const empPayrollData of empPayrollList) {
         innerHtml = `${innerHtml}   
         <tr>
@@ -27,59 +32,59 @@ const createInnerHtml = () => {
     document.querySelector('#display').innerHTML = innerHtml;
 }
 
-const createEmployeePayrollJson = () => {
-    let empPayrollListLocal = [
-        {
-            _name: "Anil Kumar",
-            _gender: "Male",
-            _department: [
-                'Engineer', 'HR'
-            ],
-            _salary: 40000,
-            _startDate: '1 Feb 2020',
-            _notes: 'Inspect',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/one.jpg'
-        },
-        {
-            _name: "Soma Shekar",
-            _gender: "Male",
-            _department: [
-                'Engineer', 'Finance'
-            ],
-            _salary: 30000,
-            _startDate: '5 Apr 2022',
-            _notes: 'Data',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/one.jpg'
-        },
-        {
-            _name: "Ankitha",
-            _gender: "Female",
-            _department: [
-                'others'
-            ],
-            _salary: 20000,
-            _startDate: '5 Aug 2021',
-            _notes: 'abc',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/person.jpg'
-        },
-        {
-            _name: "Ramya",
-            _gender: "Female",
-            _department: [
-                'Finanace', 'HR'
-            ],
-            _salary: 23000,
-            _startDate: '10 Jun 2019',
-            _notes: 'Human resource',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/person.jpg'
-        }
-    ];
-    return empPayrollListLocal;
-}
+// const createEmployeePayrollJson = () => {
+//     let empPayrollListLocal = [
+//         {
+//             _name: "Anil Kumar",
+//             _gender: "Male",
+//             _department: [
+//                 'Engineer', 'HR'
+//             ],
+//             _salary: 40000,
+//             _startDate: '1 Feb 2020',
+//             _notes: 'Inspect',
+//             _id: new Date().getTime(),
+//             _profilePic: '../assets/one.jpg'
+//         },
+//         {
+//             _name: "Soma Shekar",
+//             _gender: "Male",
+//             _department: [
+//                 'Engineer', 'Finance'
+//             ],
+//             _salary: 30000,
+//             _startDate: '5 Apr 2022',
+//             _notes: 'Data',
+//             _id: new Date().getTime(),
+//             _profilePic: '../assets/one.jpg'
+//         },
+//         {
+//             _name: "Ankitha",
+//             _gender: "Female",
+//             _department: [
+//                 'others'
+//             ],
+//             _salary: 20000,
+//             _startDate: '5 Aug 2021',
+//             _notes: 'abc',
+//             _id: new Date().getTime(),
+//             _profilePic: '../assets/person.jpg'
+//         },
+//         {
+//             _name: "Ramya",
+//             _gender: "Female",
+//             _department: [
+//                 'Finanace', 'HR'
+//             ],
+//             _salary: 23000,
+//             _startDate: '10 Jun 2019',
+//             _notes: 'Human resource',
+//             _id: new Date().getTime(),
+//             _profilePic: '../assets/person.jpg'
+//         }
+//     ];
+//     return empPayrollListLocal;
+// }
 
 const getDeptHtml = (deptList) => {
     let deptHtml = '';
@@ -87,4 +92,8 @@ const getDeptHtml = (deptList) => {
         deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`
     }
     return deptHtml;
+}
+
+const getEmpDataFromLocalStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
