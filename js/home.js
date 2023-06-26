05881437a810ed8bc62c4738c9401754421ddeb4
anilac13 <anil4u2496@gmail.com>
2023-06-26 22:18:41 +0530

    [Anil] Add:Delete record from employee payroll data

--- js/home.js
+++ js/home.js
@@ -32,59 +32,6 @@ const createInnerHtml = () => {
     document.querySelector('#display').innerHTML = innerHtml;
 }
 
-// const createEmployeePayrollJson = () => {
-//     let empPayrollListLocal = [
-//         {
-//             _name: "Anil Kumar",
-//             _gender: "Male",
-//             _department: [
-//                 'Engineer', 'HR'
-//             ],
-//             _salary: 40000,
-//             _startDate: '1 Feb 2020',
-//             _notes: 'Inspect',
-//             _id: new Date().getTime(),
-//             _profilePic: '../assets/one.jpg'
-//         },
-//         {
-//             _name: "Soma Shekar",
-//             _gender: "Male",
-//             _department: [
-//                 'Engineer', 'Finance'
-//             ],
-//             _salary: 30000,
-//             _startDate: '5 Apr 2022',
-//             _notes: 'Data',
-//             _id: new Date().getTime(),
-//             _profilePic: '../assets/one.jpg'
-//         },
-//         {
-//             _name: "Ankitha",
-//             _gender: "Female",
-//             _department: [
-//                 'others'
-//             ],
-//             _salary: 20000,
-//             _startDate: '5 Aug 2021',
-//             _notes: 'abc',
-//             _id: new Date().getTime(),
-//             _profilePic: '../assets/person.jpg'
-//         },
-//         {
-//             _name: "Ramya",
-//             _gender: "Female",
-//             _department: [
-//                 'Finanace', 'HR'
-//             ],
-//             _salary: 23000,
-//             _startDate: '10 Jun 2019',
-//             _notes: 'Human resource',
-//             _id: new Date().getTime(),
-//             _profilePic: '../assets/person.jpg'
-//         }
-//     ];
-//     return empPayrollListLocal;
-// }
 
 const getDeptHtml = (deptList) => {
     let deptHtml = '';
@@ -96,4 +43,14 @@ const getDeptHtml = (deptList) => {
 
 const getEmpDataFromLocalStorage = () => {
     return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node._id);
+    if (!empPayrollData) return;
+    const index = empPayrollList.map(empData => empData._id).indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
 }
